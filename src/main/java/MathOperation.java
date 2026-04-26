2bdff9bbc372682a2080b8d24ec7faa420262867
hungdev07 <hungnv070507@gmail.com>
2026-04-26 15:29:29 +0700

    Code sach

--- src/main/java/MathOperation.java
+++ src/main/java/MathOperation.java
@@ -1,11 +1,7 @@
 public class MathOperation {
-  public int ss(int a, int b)
-  {
-    if(a > b) return a;
+  public int SoSanh(int a, int b) {
+    if (a > b) return a;
     else if (a == b) return a;
-
-    int Result_Sum = a + b;
-
     return b;
   }
 }
